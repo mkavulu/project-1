8112d00a33b61f4ff9239af002e3c0e91996b56b
mkavulu <93429164+mkavulu@users.noreply.github.com>
2025-02-15 13:21:18 +0300

    Add files via upload

---
+++ Moses/mytype.py
@@ -0,0 +1,17 @@
+#name="John Kamau"
+#age=23
+#height=5.5
+#isMarried=True
+#print(type(age))
+#print(type(height))
+#print(type(isMarried))
+#fullName="John Kamau "
+#greeting="Hello "
+#age=34
+#print(fullName+ str(age))
+print("Enter name")
+name=input()
+print("How old are you")
+age=input()
+print("Hello "+ name+ " You are "+str(age) +" Years old")
+
